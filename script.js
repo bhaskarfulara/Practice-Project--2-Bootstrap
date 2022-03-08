96f928ffcda68c1054257063542c62e9adabd511
bhaskarfulara <bhaskar.fulara@tothenew.com>
2022-03-08 22:37:28 +0530

    final submit

---
+++ script.js
@@ -0,0 +1,19 @@
+var leftSlide=document.addEventListener('.left-slide');
+var rightSlide=document.addEventListener('.right-slide');
+
+
+leftSlide.addEventListener("click",()=>{
+    console.log("clicked")
+})
+
+
+
+
+
+
+
+
+
+
+
+
